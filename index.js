b29e7fa61d0399cacc6c0cae4002107e2ca493e2
Amandaaa <manda27122003@gmail.com>
2024-09-18 12:02:08 +0800

    added smth

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-document.title = "Latihan DOM"
-// const body = document.body
+// document.title = "Latihan DOM"
+// // const body = document.body
 
-// body.append("hello world")
+// // body.append("hello world")
 
-// let a = "Aku mau pulang";
-// console.log(a)
+// // let a = "Aku mau pulang";
+// // console.log(a)
 
-const btn1 = document.getElementById("btn1");
-const btn2 = document.querySelector(".btn2");
+// const btn1 = document.getElementById("btn1");
+// const btn2 = document.querySelector(".btn2");
 
-btn1.style.border = "none"
-btn1.style.padding = "8px";
-btn1.style.fontSize = "24px"
-btn1.style.background = "aqua"
+// btn1.style.border = "none"
+// btn1.style.padding = "8px";
+// btn1.style.fontSize = "24px"
+// btn1.style.background = "aqua"
 
-console.log(btn1)
+// console.log(btn1)
 
-function kliksaya(){
-    btn1.style.background = "tomato"
-}
+// function kliksaya(){
+//     btn1.style.background = "tomato"
+// }
 
-function  ubahText(text) {
-    btn2.textContent = text
+// function  ubahText(text) {
+//     btn2.textContent = text
     
-}
+// }
+
+var color = ['red', 'green', 'blue', 'yellow'];
+color.splice(1,2);
+console.log(color);
