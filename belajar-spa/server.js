e465485e12d4c1f138c84093b7742972ff58dff8
Amandaaa <manda27122003@gmail.com>
2024-09-23 15:09:05 +0800

    belajar spa

---
+++ belajar-spa/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+  });
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
