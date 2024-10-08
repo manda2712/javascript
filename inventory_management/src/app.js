c6fe020131cccb86cdab23c92ec10716230cd722
Amandaaa <manda27122003@gmail.com>
2024-10-08 22:58:55 +0800

    belajar membuat API

--- inventory_management/src/app.js
+++ inventory_management/src/app.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
 dotenv.config();
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 3000;  // Fallback to 3000 if undefined
+
 app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send('Hello amanda!')
-})
-app.listen(3000, () => {
-    console.log(`app listening on port` + 3000)
-})
+    res.send('Hello amanda!');
+});
+
+const authController = require("./auth/auth.controller");
+app.use("/api/auth", authController);
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ` + PORT);
+});
