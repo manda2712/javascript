bb74688aeedc37de5bf060b22038afa89ae5e2ee
Andi Amanda <135485891+manda2712@users.noreply.github.com>
2024-11-22 22:26:55 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const calculator = require(`./calculator`)
-
-const resultPenjumlahan = calculator.penjumlahan(2,6);
-const resultPengurangan = calculator.pengurangan(7,3);
-const resultPerkalian = calculator.perkalian(2,6);
-
-console.log(`Hasil penjumlaahan:  ${resultPenjumlahan}`)
-console.log(`Hasil pengurangan :  ${resultPengurangan}`)
-console.log(`Hasil perkalian :  ${resultPerkalian}`)
